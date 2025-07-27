5f0f86ad52e8ff1c28361cd6c2fad1ddc4533764
Tao Su <96sutao@gmail.com>
2025-07-26 21:28:51 -0400

    Added vans page and route

--- index.jsx
+++ index.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import {Vans} from "./pages/Vans";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-        </nav>
-      </header>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <header>
+                <Link className="site-logo" to="/">#VanLife</Link>
+                <nav>
+                    <Link to="/about">About</Link>
+                    <Link to="/vans">Vans</Link>
+                </nav>
+            </header>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/vans" element={<Vans/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 ReactDOM
-  .createRoot(document.getElementById('root'))
-  .render(<App />);
+    .createRoot(document.getElementById('root'))
+    .render(<App/>);
---
+++ pages/Vans.jsx
@@ -0,0 +1,11 @@
+function Vans() {
+  return (
+    <div>
+      <h1>Vans Page</h1>
+      <p>This is the Vans page content.</p>
+    </div>
+  );
+}
+
+export {Vans}
+
